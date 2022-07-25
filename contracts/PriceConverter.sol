0d188cb48e36d535a2f35afb1bcb9e2b8cbcec9e
0xhaz <28639279+0xhaz@users.noreply.github.com>
2022-07-25 12:12:40 +0800

    initial commit

---
+++ contracts/PriceConverter.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice(AggregatorV3Interface priceFeed)
+        internal
+        view
+        returns (uint256)
+    {
+        // ABI
+        // Address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        // AggregatorV3Interface priceFeed = AggregatorV3Interface(
+        //     0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        // );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        return uint256(answer * 1e10);
+    }
+
+    // function getVersion() internal view returns (uint256) {
+    //     AggregatorV3Interface priceFeed = AggregatorV3Interface(
+    //         0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+    //     );
+    //     return priceFeed.version();
+    // }
+
+    function getConversionRate(
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice(priceFeed);
+        // 3000_00000000000000 = ETH / USD Price
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
